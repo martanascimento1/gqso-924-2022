cfa4840338f8b9abfcd865228e26c39deda7f8c5
martanascimento1 <mmns5@aluno.ifal.edu.br>
2022-06-21 16:55:35 +0000

    Implementação da função de divisão.

---
+++ Calcdiv.java
@@ -0,0 +1,46 @@
+import static java.net.HttpURLConnection.HTTP_OK;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class Calcdiv implements HttpHandler {
+    public static final String PATH = "/div";
+
+    @Override
+    public void handle(HttpExchange conn) throws IOException {
+        String[] partes = conn.getRequestURI().getPath().split("/");
+
+        String parametro = partes[3];
+        String parametro1 = partes[4];
+        byte[] result = calculateResponse(parametro, parametro1);
+
+        try {
+        
+            conn.sendResponseHeaders(HTTP_OK, result.length);
+            Headers headers = conn.getResponseHeaders();
+            headers.add("Content-Type", "text/html; charset=UTF-8");
+            try (OutputStream out = conn.getResponseBody()) {
+                out.write(result);
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.close();
+        }
+    }
+
+    byte[] calculateResponse(String parametro, String parametro1) {
+        double pDouble = Double.parseDouble(parametro);
+        double pDouble2 = Double.parseDouble(parametro1);
+
+        double divisao = (pDouble / pDouble2);
+
+        return Double.toString(divisao).getBytes();
+    }
+}
