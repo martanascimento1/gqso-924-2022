bbf7819655c3b8b04ffd1d7148aace02129c2cce
martanascimento1 <103062784+martanascimento1@users.noreply.github.com>
2022-06-21 17:23:25 -0300

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,25 +1,26 @@
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
-import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
 
-public class Server {
+public class Servidor {
 
     public static final int TAMANHO_BACKLOG = 100;
 
     public static void main(String[] args) throws IOException {
-        InetSocketAddress bindAddr = new InetSocketAddress(3000);
+
+        InetSocketAddress bindAddr = new InetSocketAddress(36615);
         HttpServer server = HttpServer.create(bindAddr, TAMANHO_BACKLOG);
         server.setExecutor(Executors.newSingleThreadExecutor());
 
+        server.createContext(IndexHandler.PATH, new IndexHandler());
         server.createContext(CalcSoma.PATH, new CalcSoma());
         // server.createContext(.PATH, new );
         // server.createContext(.PATH, new );
         // server.createContext(.PATH, new );
 
+
         server.start();
         System.out.printf("Servidor ouvindo requisições na porta %s\n\n", server.getAddress().getPort());
-    }
-}
