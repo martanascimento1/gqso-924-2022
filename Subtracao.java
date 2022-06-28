5aa5b888ee5d0add6e784e119c23824de88adc2f
Kauan Nogueira <kna1@aluno.ifal.edu.br>
2022-06-28 17:05:23 +0000

    alteração sub

--- Subtracao.java
+++ Subtracao.java
@@ -6,25 +6,19 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class Subtracao implements HttpHandler {
-
     public static final String PATH = "/sub";
     
     @Override
     public void handle(HttpExchange conn) throws IOException {
-
         String[] partes = conn.getRequestURI().getPath().split("/");
-
         String operador1 = partes[2]; // partes[2] = primeiro número digitado para somar
         String operador2 = partes[3]; // parte [3] = segundo número digitado para somar
-
         byte[] result = calculateResponse(operador1, operador2);
 
         try {
             conn.sendResponseHeaders(HTTP_OK, result.length);
-
             Headers headers = conn.getResponseHeaders();
             headers.add("Content-Type", "text/html; charset=UTF-8");
-
             try (OutputStream out = conn.getResponseBody()) {
                 out.write(result);
             } catch (IOException ioe) {
@@ -36,14 +30,10 @@ public class Subtracao implements HttpHandler {
             conn.close();
         }
     }
-
     byte[] calculateResponse(String operador1, String operador2) {
-
         double numero1 = Double.parseDouble(operador1);
         double numero2 = Double.parseDouble(operador2);
-
         double subtracao = (numero1 - numero2);
-
         return Double.toString(subtracao).getBytes();
     }
 }
